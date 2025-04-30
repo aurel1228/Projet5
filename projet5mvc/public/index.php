23b9d77bbbaccb3b62164353f237e2b7e2a727c4
aurel1228 <nasusdu44@gmail.com>
2025-04-30 18:13:23 +0200

    Add files via upload

--- projet5mvc/public/index.php
+++ projet5mvc/public/index.php
@@ -1,10 +1,44 @@
-<?php   
-
-
-if($url == '') {
-    require 'accueil.php';
-} else {
-    require '404.php';
+<?php
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    http_response_code(404);
+    die();
 }
 
-?>
+class Page
+{
+    protected static bool $Return_404 = true;
+    protected static function Return_404(): void
+    {
+        (file_exists("./view/404.php")) ?  require_once "./view/404.php" : http_response_code(404);
+    }
+    protected static function File(string $file): string
+    {
+        if (!empty($file)) {
+            (str_contains($file, "?")) ? $file = strtok($file, '?') : $file;
+            ($file[strlen($file) - 1] === "/") ? $file = rtrim($file, "/") : $file;
+        }
+        return $file;
+    }
+}
+class Router extends Page
+{
+    public static function Run(): void
+    {
+        $requested_file = self::File($_SERVER["REQUEST_URI"]);
+        foreach (Routes::$Route as $request => $file) {
+            if ($requested_file === $request) {
+                if (file_exists($file)) {
+                    self::$Return_404 = false;
+                    require $file;
+                } else echo "Error";
+            }
+        }
+        if (self::$Return_404) self::Return_404();
+    }
+}
+class Routes
+{
+    public static array $Route = array(
+        "" => "./view/accueil.php",
+    );
+}
