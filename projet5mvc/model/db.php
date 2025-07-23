38cc97db6a3cbbc1c793862a165d145cc2a57c96
aurel1228 <nasusdu44@gmail.com>
2025-07-23 18:15:02 +0200

    Add files via upload

---
+++ projet5mvc/model/db.php
@@ -0,0 +1,22 @@
+<?php
+   $env = parse_ini_file(__DIR__.'/../.env');
+   
+   $username=$env['user_name'];
+   $password=$env['password'];
+   $db_dsn=$env['db_dsn'];
+   
+   try {
+    $conn = new PDO($db_dsn,$username,$password);
+    echo 'Connexion réussie';
+   }
+   catch (PDOException $exception){
+    echo 'connexion echoué:'.$exception->getMessage();
+    $conn = null;
+   }
+   
+   //if (!$conn) {
+   //  echo "connexion échoué";
+   //}
+   //echo 'Connexion réussie';
+   
+   ?>
--- projet5mvc/public/index.php
+++ projet5mvc/public/index.php
@@ -1,19 +1,21 @@
 <?php
-session_start();
-$request = $_SERVER['REQUEST_URI'];
-$controllerDir = __DIR__.'/../controllers/';
-switch ($request) {
-    case '':
-    case '/':
-        require $controllerDir . 'accueil.php';
-        break;
-
-    case '/views/admin':
-        require $controllerDir . 'admin.php';
-        break;
-
-    default:
-        http_response_code(404);
-        require $controllerDir . 'error.php';
-}
-?>
+   session_start();
+   $request = $_SERVER['REQUEST_URI'];
+   require __DIR__."/../model/db.php";  
+   $controllerDir = __DIR__.'/../controllers/';
+   switch ($request) {
+       case '':
+       case '/':
+           require $controllerDir . 'accueil.php';
+           break;
+   
+       case '/views/admin':
+           require $controllerDir . 'admin.php';
+           break;
+   
+       default:
+           http_response_code(404);
+           require $controllerDir . 'error.php';
+   }
+   
+   ?>
