e21029629c7af983b7eb8ea13be147ec1d981a97
aurel1228 <nasusdu44@gmail.com>
2026-02-11 19:47:09 +0100

    Delete projet5mvc/views/error.php

--- projet5mvc/views/error.php
+++
@@ -1,6 +0,0 @@
-<?php /* $titre = "page d'erreur"; require __DIR__ . "/_head.php"; */?>
-   <body>
-      Erreur 404  
-      <a href="/" id="retour">retour à la page d'accueil</a>
-   </body>
-</html>
