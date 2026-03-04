5dc991096178f469cd355cd53ad65784ac0e2462
aurel1228 <nasusdu44@gmail.com>
2026-03-04 19:49:03 +0100

    Add files via upload

--- projet5mvc/Public/js/Home.js
+++ projet5mvc/Public/js/Home.js
@@ -1,4 +1,4 @@
-document.getElementById("search-btn").addEventListener("click", () => {
+document.getElementById("search-btn").addEventListener("click", (event) => {
   const city = document.getElementById("city-input").value;
   console.log(city);
   if (city) {
@@ -22,10 +22,103 @@ function fetchWeather(city) {
     .catch(error => displayError(error.message));
 }
 
+const iderror = document.getElementById("error");
+
 function displayWeather(data) {
-  document.getElementById("error-message").textContent = "";
-  document.getElementById("city-name").textContent = `Weather in ${data.name}`;
-  document.getElementById("temperature").textContent = `Temperature: ${data.main.temp}°C`;
-  document.getElementById("description").textContent = `Condition: ${data.weather[0].description}`;
-  document.getElementById("humidity").textContent = `Humidity: ${data.main.humidity}%`;
+  iderror.textContent = "";
+  let html;
+  switch(data.weather[0].main){ 
+    case "Clear":
+      html=`<div class="icon sunny">
+         <div class="sun">
+            <div class="rays"></div>
+         </div>
+      </div>`;
+  }
+  document.getElementById("apimeteo").innerHTML= html;
+
 }
+
+function displayError(error) {
+  iderror.textContent = error;
+}
+
+
+
+
+ /*    <?php 
+         switch($weather)
+         {
+             case "Clear":
+                 ?>
+      <div class="icon sunny">
+         <div class="sun">
+            <div class="rays"></div>
+         </div>
+      </div>
+      <?php
+         break;
+         
+         case 'Drizzle':
+         ?>
+      <div class="icon sun-shower">
+         <div class="cloud"></div>
+         <div class="sun">
+            <div class="rays"></div>
+         </div>
+         <div class="rain"></div>
+      </div>
+      <?php 
+         break;
+         
+         case 'Rain':
+         ?>
+      <div class="icon rainy">
+         <div class="cloud"></div>
+         <div class="rain"></div>
+      </div>
+      <?php 
+         break;
+         
+         case 'Clouds':
+         ?>
+      <div class="icon cloudy">
+         <div class="cloud"></div>
+         <div class="cloud"></div>
+      </div>
+      <?php 
+         break;
+         
+         case 'Thunderstorm':
+         ?>
+      <div class="icon thunder-storm">
+         <div class="cloud"></div>
+         <div class="lightning">
+            <div class="bolt"></div>
+            <div class="bolt"></div>
+         </div>
+      </div>
+      <?php 
+         break;
+         
+         case 'Snow':
+         ?>
+      <div class="icon flurries">
+         <div class="cloud"></div>
+         <div class="snow">
+            <div class="flake"></div>
+            <div class="flake"></div>
+         </div>
+      </div>
+      <?php 
+         break;
+         }
+         ?>
+      <div class="meteo_desc text-left">
+         <h2>
+            <?php echo $temp; ?> °C / Ressenti <?php echo $feel_like; ?> °C <br />
+            <?php echo $speed; ?> Km/h - <?php echo $deg; ?> ° <br /> 
+            <?php echo $desc; ?>
+         </h2>
+      </div>
+      */
