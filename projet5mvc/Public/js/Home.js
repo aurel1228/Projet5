49c1249dc8aebcc7c0caad765d6834a1acafebba
aurel1228 <nasusdu44@gmail.com>
2026-03-17 20:50:59 +0100

    Add files via upload

--- projet5mvc/Public/js/Home.js
+++ projet5mvc/Public/js/Home.js
@@ -1,3 +1,9 @@
+const p = trustedTypes.createPolicy('inner', {
+   createHTML: function (input){
+      return DOMPurify.sanitize(input);
+   }
+});
+
 document.getElementById("search-btn").addEventListener("click", (event) => {
   const city = document.getElementById("city-input").value;
   console.log(city);
@@ -26,6 +32,11 @@ const iderror = document.getElementById("error");
 
 function displayWeather(data) {
   iderror.textContent = "";
+  const temp =  data.main.temp;
+  const feel_like = data.main.feel_like;
+  const speed = data.wind.speed;
+  const deg = data.wind.deg;
+  console.log(temp);
   let html;
   switch(data.weather[0].main){ 
     case "Clear":
@@ -34,17 +45,55 @@ function displayWeather(data) {
             <div class="rays"></div>
          </div>
       </div>`;
+      break;
+      case "Drizzle":
+         html=`<div class="icon sun-shower">
+         <div class="cloud"></div>
+         <div class="sun">
+            <div class="rays"></div>
+         </div>
+         <div class="rain"></div>
+      </div>`;
+      break;
+      case "Rain":
+         html=`<div class="icon rainy">
+         <div class="cloud"></div>
+         <div class="rain"></div>
+      </div>`;
+      break;
+      case "Clouds":
+         html=`<div class="icon cloudy">
+         <div class="cloud"></div>
+         <div class="cloud"></div>
+      </div>`;
+      break;
+      case "Thunderstorm":
+         html=`<div class="icon thunder-storm">
+         <div class="cloud"></div>
+         <div class="lightning">
+            <div class="bolt"></div>
+            <div class="bolt"></div>
+         </div>
+      </div>`;
+      break;
+      case "Snow":
+         html=`<div class="icon flurries">
+         <div class="cloud"></div>
+         <div class="snow">
+            <div class="flake"></div>
+            <div class="flake"></div>
+         </div>
+      </div>`;
+      break;
   }
-  document.getElementById("apimeteo").innerHTML= html;
+document.getElementById("apimeteo").innerHTML= p.createHTML(html);
 
 }
 
 function displayError(error) {
   iderror.textContent = error;
 }
-
-
-
+document.getElementById("temp").textcontent="température";
 
  /*    <?php 
          switch($weather)
