b57767758c0cf2e77422c13bead29b2ee5a62de3
aurel1228 <nasusdu44@gmail.com>
2026-04-14 19:53:41 +0200

    changement dossier

--- projet5mvc/Controllers/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Projet5\Controllers;
-class Logout extends AbstractUserController{
-   public function process():void{
-      session_unset();
-      header("location: /");
-      exit();
-   }
-}
