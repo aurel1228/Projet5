492e9896717a87595fb5edc6d34acedbb6faa34e
aurel1228 <nasusdu44@gmail.com>
2026-07-07 20:30:09 +0200

    pagination analyse

--- Controllers/Admin/Users.php
+++ Controllers/Admin/Users.php
@@ -6,9 +6,12 @@ use Projet5\Tools\RoleEnum;
 class Users extends AbstractViewController {
     private const MAX=10;
     public function process():void{
-        $this->variableView["Users"]=User::getPage(1, self::MAX); //préparer ? pour choisir le numéro de la page mais pas le start
-
-        var_dump(ceil(User::userCount()/self::MAX)); 
+        $totalPages=(ceil(User::userCount()/self::MAX));
+        $currentpage=1;
+      for ($i = 1; $i <= $totalPages; $i++) { 
+            echo($i);
+        }
+        $this->variableView["Users"]=User::getPage($i, self::MAX);
         parent::process();  
     }
 
@@ -16,4 +19,6 @@ class Users extends AbstractViewController {
         return RoleEnum::Admin;
     }
 }
+//        $this->variableView["Users"]=User::getPage(1, self::MAX); //préparer ? pour choisir le numéro de la page mais pas le start
+//       var_dump(ceil(User::userCount()/self::MAX)); 
 ?>
