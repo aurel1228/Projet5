60f99e8f5c5c162d376363dc11dff241e2c6b4ba
aurel1228 <nasusdu44@gmail.com>
2026-07-29 19:52:51 +0200

    correctif

--- Controllers/AbstractViewController.php
+++ Controllers/AbstractViewController.php
@@ -10,7 +10,7 @@ abstract class AbstractViewController extends AbstractUserController implements 
         $viewName=(new ReflectionClass($this))->getName(); //permet de récupérer le nom de la classe
         $viewName=mb_substr($viewName, mb_strlen("Projet5\\Controllers\\")); //récupère le nom de la classe et enlève projet5/controllers
         $twigLoader=new \Twig\Loader\FilesystemLoader(); 
-        $twigLoader->prependPath(__DIR__."/../views");
+        $twigLoader->prependPath(__DIR__."/../Views");
         $enviLoader=new \Twig\Environment($twigLoader, ["cache"=>false]); // créer environnement twig
         $htmlView= $enviLoader->render($viewName.".html.twig",$this->variableView + ["SESSION"=>$_SESSION]);// ajouter a twig des variables de la vue et $Session
         echo $htmlView;
