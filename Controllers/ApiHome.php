3a008a03105288ddbdc991a260a771d4108bfbc1
aurel1228 <nasusdu44@gmail.com>
2026-08-04 19:38:00 +0200

    correction erreur api meteo

--- Controllers/ApiHome.php
+++ Controllers/ApiHome.php
@@ -5,6 +5,10 @@ class ApiHome extends AbstractUserController{
         $env = parse_ini_file(__DIR__.'/../.env');
         $url = "http://api.openweathermap.org/data/2.5/weather?q=".$_GET["city"]."&lang=fr&units=metric&appid=".$env['API_KEY'];
         $raw = file_get_contents($url);
+        if ($raw === false){
+            http_response_code(500);
+            exit();
+        }
         header("content-type: application/json; charset=utf-8");
         echo $raw;
         exit();
